9f3b27d849c0bdd8ccc8ace4e7aeb7cba523c79f
MohamedAnushkar <anush@doubleq.com.au>
2023-01-19 18:18:40 +0530

    final version mobile responsive

--- src/pages/album/album.js
+++ src/pages/album/album.js
@@ -8,20 +8,23 @@ const Album = () => {
   const preview = (url) => setURL(url);
 
   return (
-    <div className="container max-w-[900px]">
-      <div className="flex items-start justify-between">
+    <div className="container max-w-[900px] px-4">
+      <div className="flex items-start justify-between  bg-white">
         <h2 class="text-stone-800 mb-4 my-3">Images</h2>
         <Link to={"/"} class="text-stone-800 mb-4 my-3 cursor-pointer">
           Back
         </Link>
       </div>
 
-      <div className="flex items-start">
-        <motion.div layout className="grid grid-cols-4 gap-4">
-          {data.map((dt) => (
-            <motion.div layout>
+      <div className="flex md:flex-row flex-col-reverse items-start">
+        <motion.div
+          layout
+          className="grid grid-cols-3 sm:grid-cols-4 gap-4 w-full"
+        >
+          {data.map((dt, i) => (
+            <motion.div key={i} layout>
               <img
-                className="transition-all hover:scale-105 hover:z-10 w-32 cursor-pointer "
+                className="transition-all hover:scale-105 hover:z-10 w-full cursor-pointer "
                 src={dt.thumbnailUrl}
                 alt={dt.title}
                 onClick={() => preview(dt.url)}
@@ -29,18 +32,16 @@ const Album = () => {
             </motion.div>
           ))}
         </motion.div>
-        <div className="inline-block ml-auto w-72 h-72 shrink-0 sticky top-4">
-          {url ? (
-            <img
-              className="inline-block w-full h-full "
-              src={url}
-              alt="Preview"
-            />
-          ) : (
-            <p className="w-full h-full bg-gray-300 flex items-center justify-center">
-              Please selects an image
-            </p>
-          )}
+        <div className="inline-block md:ml-4 md:w-72 w-full h-56 md:h-auto md:aspect-square shrink-0 sticky md:top-4 top-0 bg-white  pb-4">
+          <div className="w-52 md:w-full h-full">
+            {url ? (
+              <img className="inline-block" src={url} alt="Preview" />
+            ) : (
+              <p className="w-full h-full bg-gray-300 flex items-center justify-center">
+                Please selects an image
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/albums/albums.js
+++ src/pages/albums/albums.js
@@ -5,11 +5,11 @@ const Albums = () => {
   const data = useLoaderData();
 
   return (
-    <div className="container max-w-[900px]">
+    <div className="container max-w-[900px] px-4">
       <h2 class="text-stone-800 mb-4 my-3">Albums</h2>
       <ul>
         {data.map((dt, i) => (
-          <li className="my-2 hover:underline">
+          <li key={i} className="my-2 hover:underline">
             <Link to={`/${dt.id}`}>Album{i + 1}</Link>
           </li>
         ))}
